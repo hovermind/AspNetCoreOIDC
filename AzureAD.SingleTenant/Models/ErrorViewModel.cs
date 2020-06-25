7ed0f42ded02f0fc62b0934edfce04bc61338562
HASSAN ハッサン <console041@gmail.com>
2020-06-25 18:34:20 +0900

    Add project files.

---
+++ AzureAD.SingleTenant/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAD.SingleTenant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
