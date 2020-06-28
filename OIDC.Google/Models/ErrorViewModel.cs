def621a5da2d7d83b7782c3aea6b5f215d0047c0
HASSAN ハッサン <console041@gmail.com>
2020-06-29 03:15:29 +0900

    Add google authentication

---
+++ OIDC.Google/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OIDC.Google.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
